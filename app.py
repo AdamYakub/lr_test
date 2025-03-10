8c0832fa42cc51e7713e37ac27c98873f0c7eb7e
Adam Yakub <117546979+AdamYakub@users.noreply.github.com>
2025-03-10 22:03:51 +0100

    Add files via upload
    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import pickle
+
+#data = pd.read_csv('customers.csv')
+
+
+
+#st.write(data)
+st.title('welcome to Model Deployement page')
+
+model = pickle.load(open('lr_model.pkl','rb'))
+
+feature1 = st.sidebar.number_input("Enter Feature 1")
+feature2 = st.sidebar.number_input("Enter Feature 2")
+feature3 = st.sidebar.number_input("Enter Feature 3")
+feature4 = st.sidebar.number_input("Enter Feature 4")
+feature5 = st.sidebar.number_input("Enter Feature 5")
+
+input_data = np.array([feature1,feature2,feature3,feature4,feature5])
+input_data = input_data.reshape(1, -1)
+
+st.write('Prediction')
+if st.button('prediction') is not None:
+    prediction = model.predict(input_data)
+    st.write(f"The price {prediction[0]}")
+
+
+
